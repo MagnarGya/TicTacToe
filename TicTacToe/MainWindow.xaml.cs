431da1e078e7020409ce86d12f412dee9a208667
Magnar Gya <magnar.gya@hotmail.com>
2017-11-30 14:03:00 +0100

    Fixed all winning conditions, except cross.

--- TicTacToe/MainWindow.xaml.cs
+++ TicTacToe/MainWindow.xaml.cs
@@ -38,6 +38,7 @@ namespace TicTacToe
         {
             buttons = new Button[game.Size * game.Size];
             Grid grid = (Grid)this.FindName("Game");
+            grid.VerticalAlignment = VerticalAlignment.Stretch;
             grid.Children.Clear();
             grid.ColumnDefinitions.Clear();
             grid.RowDefinitions.Clear();
@@ -45,6 +46,14 @@ namespace TicTacToe
             {
                 ColumnDefinition col = new ColumnDefinition();
                 RowDefinition row = new RowDefinition();
+                if (double.IsNaN(grid.Height))
+                {
+                    row.Height = new GridLength(285 / game.Size);
+                }
+                else
+                {
+                    row.Height = new GridLength(grid.Height / game.Size);
+                }
                 grid.ColumnDefinitions.Add(col);
                 grid.RowDefinitions.Add(row);
             }
@@ -65,11 +74,78 @@ namespace TicTacToe
             Button button = (Button)sender;
             button.Content = currentplayer;
             currentplayer = (currentplayer + 1) % playermax;
+            button.IsEnabled = false;
             checkGame();
         }
+
         void checkGame()
         {
+            bool gameOver = false;
+            int match = 0;
 
+            //Checks Rows for winning condition
+            for(int i = 0; i < game.Size-1; i++)
+            {
+                for(int j = 1; j < game.Size; j++)
+                {
+                    if (buttons[i * game.Size + j - 1].Content.Equals(buttons[i * game.Size + j].Content) && !buttons[i * game.Size + j].Content.Equals("-"))
+                    {
+                        match++;
+                    }
+                }
+                if (match == game.Size - 1)
+                {
+                    gameOver = true;
+                }
+                match = 0;
+            }
+
+            //Checks Columns for winning condition
+            for(int i = 0; i < game.Size; i++)
+            {
+                for(int j = 1; j < game.Size; j++)
+                {
+                    if (buttons[(j-1) * game.Size + i].Content.Equals(buttons[j * game.Size + i].Content) && !buttons[j * game.Size + i].Content.Equals("-"))
+                    {
+                        match++;
+                    }
+                }
+                if (match == game.Size - 1)
+                {
+                    gameOver = true;
+                }
+                match = 0;
+            }
+
+            //Checks Cross for winning condition
+            for(int i = 1; i < game.Size; i++)
+            {
+                if (buttons[game.Size * (i - 1) + (i - 1)].Content.Equals(buttons[game.Size * i + i]) && !buttons[game.Size * i + i].Content.Equals("-"))
+                {
+                    match++;
+                }
+            }
+            if (match == game.Size - 1)
+            {
+                gameOver = true;
+            }
+            match = 0;
+            for(int i = 2; i < game.Size+1; i++)
+            {
+                if(buttons[game.Size * (i-1) - (i-1)].Content.Equals(buttons[game.Size * i - i]) && !buttons[game.Size * i - i].Content.Equals("-"))
+                {
+                    match++;
+                }
+            }
+
+
+            if (gameOver)
+            {
+                for(int i = 0; i < buttons.Length; i++)
+                {
+                    buttons[i].IsEnabled = false;
+                }
+            }
         }
 
         private void Button_NewGame(object sender, RoutedEventArgs e)
