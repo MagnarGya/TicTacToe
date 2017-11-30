1efd9747255fc3fd2a38a298d6f8cf5e16a5e50f
Magnar Gya <magnar.gya@hotmail.com>
2017-11-30 12:59:53 +0100

    Almost finished with the visual part of the game.

--- TicTacToe/MainWindow.xaml.cs
+++ TicTacToe/MainWindow.xaml.cs
@@ -20,9 +20,72 @@ namespace TicTacToe
     /// </summary>
     public partial class MainWindow : Window
     {
+        Game game;
+        Button[] buttons;
+        int playermax;
+        int currentplayer;
+
         public MainWindow()
         {
             InitializeComponent();
+            game = new Game(3);
+            playermax = 2;
+            currentplayer = 0;
+            NewGame();
         }
+
+        public void NewGame()
+        {
+            buttons = new Button[game.Size * game.Size];
+            Grid grid = (Grid)this.FindName("Game");
+            grid.Children.Clear();
+            grid.ColumnDefinitions.Clear();
+            grid.RowDefinitions.Clear();
+            for(int i = 0; i < game.Size; i++)
+            {
+                ColumnDefinition col = new ColumnDefinition();
+                RowDefinition row = new RowDefinition();
+                grid.ColumnDefinitions.Add(col);
+                grid.RowDefinitions.Add(row);
+            }
+            for (int i = 0; i < buttons.Length; i++)
+            {
+                buttons[i] = new Button();
+                buttons[i].Content = "-";
+                buttons[i].Click += new RoutedEventHandler(this.ButtonClicked);
+                Grid.SetRow(buttons[i], (i / game.Size));
+                Grid.SetColumn(buttons[i], (i % game.Size));
+                grid.Children.Add(buttons[i]);
+            }
+        }
+
+
+        void ButtonClicked(Object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            button.Content = currentplayer;
+            currentplayer = (currentplayer + 1) % playermax;
+            checkGame();
+        }
+        void checkGame()
+        {
+
+        }
+
+        private void Button_NewGame(object sender, RoutedEventArgs e)
+        {
+            NewGame();
+        }
+    }
+}
+
+    public class Game
+{
+    private int m_size;
+    public int Size { get { return m_size; } }
+
+    public Game(int _size)
+    {
+        m_size = _size;
     }
 }
